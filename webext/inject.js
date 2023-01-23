03a100a5c5d1096b16b876c153a36abc45bf8f66
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-23 13:03:01 -0500

    Make userscript web accessible for Chrome

--- webext/inject.js
+++ webext/inject.js
@@ -1,4 +1,5 @@
 // create <script> tag with content of userscript.js and add it to the DOM
+console.log('[R2 Video User Markers] Injecting dist/userscript.js...')
 var script = document.createElement('script');
 script.src = chrome.extension.getURL('dist/userscript.js');
 (document.head || document.documentElement).appendChild(script);
